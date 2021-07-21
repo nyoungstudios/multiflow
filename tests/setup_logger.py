cc34088be8f211f444d885fa92b0ff42baf39bec
Nathaniel Young <nathanielyoung95070@gmail.com>
2021-07-21 07:49:24 -0700

    adds seperate file for logger

---
+++ tests/setup_logger.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+def get_logger(name, log_format=LOG_FORMAT):
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    return logger

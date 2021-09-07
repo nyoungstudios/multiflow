e3711a1a40d130617e4ffa1f0e1787f5c5a88e8c
Nathaniel Young <nathanielyoung95070@gmail.com>
2021-09-07 01:29:37 -0700

    improves test coverage

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -839,7 +839,7 @@ class TestFlowFlowBase(TestFlowBase):
             flow.add_function(add_n, n=5)
 
             for output in flow:
-                self.assertEqual(5, output.get('n'))
+                self.assertEqual(5, getattr(output, 'n'))
                 self.assertEqual(output[0] + 5, output.get_result())
 
             count = flow.get_successful_job_count()
